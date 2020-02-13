59b4aa312ab2586b85a01fd1087e1beee3eb9770
coskunyerli <coskun.yerli@gmail.com>
2020-02-13 20:35:09 +0300

    multiple selection support

--- app/src/main/java/com/scribenoteapp/scribe/adapter/NoteAdapter.java
+++ app/src/main/java/com/scribenoteapp/scribe/adapter/NoteAdapter.java
@@ -1,10 +1,8 @@
 package com.scribenoteapp.scribe.adapter;
 
 import android.content.Context;
-import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,32 +12,32 @@ import android.widget.TextView;
 import com.scribenoteapp.scribe.R;
 import com.scribenoteapp.scribe.framework.ModelIndex;
 import com.scribenoteapp.scribe.model.NoteModel;
-import com.scribenoteapp.scribe.model.note.BaseNote;
-import com.scribenoteapp.scribe.model.note.Note;
-import com.scribenoteapp.scribe.model.note.NoteFolder;
-
-import java.util.ArrayList;
 
 /**
  * Created by ALLDe on 13/01/2020.
  */
 
-public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder>{
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
 
     private NoteModel model;
     private ListItemClickListener listItemClickListener;
+    private boolean[] selectedIndexes;
 
-    public interface ListItemClickListener{
+    public interface ListItemClickListener {
         void onListItemClick(ModelIndex clickedItemIndex, View view);
     }
 
-    public NoteAdapter(NoteModel model)
-    {
-        this.model= model;
+    public NoteAdapter(NoteModel model) {
+        this.model = model;
+        this.selectedIndexes = new boolean[this.model.rowCount()];
     }
 
-    public void setOnListItemClickListener(ListItemClickListener listItemClickListener)
-    {
+    public boolean[] getSelectedIndexes(){
+        return this.selectedIndexes;
+    }
+
+
+    public void setOnListItemClickListener(ListItemClickListener listItemClickListener) {
         this.listItemClickListener = listItemClickListener;
     }
 
@@ -60,43 +58,47 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
         return model.rowCount();
     }
 
+
     @Override
     public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
         holder.bind(this.model.index(position, 0));
     }
 
-    public class NoteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
-    {
+    public class NoteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView noteTitle;
         private TextView noteBody;
         private TextView noteDate;
         private ImageView noteIcon;
 
-        public NoteViewHolder(View noteView)
-        {
+        public NoteViewHolder(final View noteView) {
             super(noteView);
             this.noteTitle = noteView.findViewById(R.id.note_title_hint);
             this.noteBody = noteView.findViewById(R.id.note_body_hint);
             this.noteDate = noteView.findViewById(R.id.note_date);
             this.noteIcon = noteView.findViewById(R.id.note_icon);
+
             noteView.setOnClickListener(this);
+            this.noteIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    NoteAdapter.this.selectedIndexes[getAdapterPosition()] = !NoteAdapter.this.getSelectedIndexes()[getAdapterPosition()];
+                    notifyItemChanged(position);
+                }
+            });
         }
 
-        public void bind(ModelIndex index)
-        {
-            BaseNote note = model.getItem(index);
-            this.noteTitle.setText(note.getTitle());
-            this.noteDate.setText(note.getCreationDate());
+        public void bind(ModelIndex index) {
+            this.noteTitle.setText((String) index.siblingAtColumn(0).data());
+            this.noteBody.setText((String) index.siblingAtColumn(1).data());
+            this.noteDate.setText((String) index.siblingAtColumn(2).data());
 
-            if (note instanceof NoteFolder)
-            {
-                this.noteBody.setText(null);
-                this.noteIcon.setImageResource(R.drawable.ic_folder_icon);
-            }
-            else if (note instanceof Note)
-            {
-                this.noteBody.setText(index.data());
-                this.noteIcon.setImageResource(R.drawable.ic_note_icon);
+            if (NoteAdapter.this.selectedIndexes[getAdapterPosition()]) {
+                this.itemView.setBackgroundColor(this.itemView.getResources().getColor(R.color.noteItemBackgroundSelectedColor));
+                this.noteIcon.setImageResource(R.drawable.ic_check_circle_black_24dp);
+            } else {
+                this.itemView.setBackgroundColor(this.itemView.getResources().getColor(R.color.noteItemBackgroundColor));
+                this.noteIcon.setImageResource((Integer) index.siblingAtColumn(3).data());
             }
         }
 
