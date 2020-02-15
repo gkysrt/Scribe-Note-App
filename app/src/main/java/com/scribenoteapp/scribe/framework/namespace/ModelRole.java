4d05ffe0975ec9501966c3ce22aea3c6291b51f8
coskunyerli <coskun.yerli@gmail.com>
2020-02-15 12:26:51 +0300

    name is changed of flags

--- app/src/main/java/com/scribenoteapp/scribe/framework/namespace/ModelRole.java
+++ app/src/main/java/com/scribenoteapp/scribe/framework/namespace/ModelRole.java
@@ -4,6 +4,6 @@ package com.scribenoteapp.scribe.framework.namespace;
  * Created by ALLDe on 16/01/2020.
  */
 
-public class  ItemDataModel {
+public class ModelRole {
     public final static int DISPLAY_ROLE = 1, EDIT_ROLE = 2, USER_ROLE = 4;
 }
