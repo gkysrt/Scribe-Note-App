2c052a2b19dd8011f02df9bdfc121dc210654542
coskunyerli <coskun.yerli@gmail.com>
2020-02-15 12:25:44 +0300

    implemented all, exists methods

---
+++ app/src/main/java/com/scribenoteapp/scribe/Static.java
@@ -0,0 +1,23 @@
+package com.scribenoteapp.scribe;
+
+import com.scribenoteapp.scribe.framework.slots.Function1;
+
+public class Static {
+    public static <T> boolean all(T[] list, Function1<T, Boolean> function) {
+        for (T item : list) {
+            if (!function.function(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static <T> boolean exists(T[] list, Function1<T, Boolean> function) {
+        for (T item : list) {
+            if (function.function(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
